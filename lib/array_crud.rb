2ade255d02845d8184dff945f7bc414e1eef839a
Nate Croteau <natekcroteau@gmail.com>
2020-11-16 21:38:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array<< "new puppy"
+  croteau_family = ["nate", "erica", "lucy", "flynn"]
+  croteau_family.push "new pup"
 end
 
 def add_element_to_start_of_array(array, element)
