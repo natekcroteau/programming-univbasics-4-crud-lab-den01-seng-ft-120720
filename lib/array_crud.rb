80d6d0739f66da6e402477b6fdad62918c41ab81
Nate Croteau <natekcroteau@gmail.com>
2020-11-16 21:55:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,6 +10,7 @@ def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
   element = "arrays"
   array << element
+  p array
 end
 
 def add_element_to_start_of_array(array, element)
