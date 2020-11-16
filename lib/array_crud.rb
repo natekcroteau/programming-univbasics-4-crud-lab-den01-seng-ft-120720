208c80477c13a2e654c2dd0f1d4d4927c4583f27
Nate Croteau <natekcroteau@gmail.com>
2020-11-16 21:52:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  numbers = [1,2,3,4]
-  numbers << 6
+  numbers = ['1','2','3','4']
+  numbers << '6'
   p numbers
 end
 
